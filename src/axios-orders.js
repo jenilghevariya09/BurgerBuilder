37ea91b0fd6f1be82118c3278e39937e5379514e
Jenil Ghevariya <jenilghevariya000@gmail.com>
2023-01-10 12:04:47 +0530

    firebase url

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
  const instance = axios.create({
-    baseURL: "https://react--burger-builder-d48bc-default-rtdb.asia-southeast1.firebasedatabase.app"
+    baseURL: "//add your Firebase Realtime Database URL"
 });
 
 export default instance;
